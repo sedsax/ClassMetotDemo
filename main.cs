63a1e267528deddddbb3b4a1ae4cf19518a6fd8a
Sedanur Savaş <44877350+sedsax@users.noreply.github.com>
2021-02-27 15:30:22 +0300

    Update main.cs

--- main.cs
+++ main.cs
@@ -7,17 +7,56 @@ MusteriManager sınıfı oluşturunuz. Musteri parametresi alarak Musteri ekleme
 
 class MainClass {
   public static void Main (string[] args) {
-    Console.WriteLine ("Hello World");
+    Musteri m1 = new Musteri();
+    m1.id=1001;
+    m1.ad = "Selin";
+    m1.soyad = "Kerimoğlu";
+            
+    Musteri m2 = new Musteri();
+    m2.id=1007;
+    m2.ad = "Menderes";
+    m2.soyad = "Mermertay";
+
+    Musteri m3 = new Musteri();
+    m3.id=1023;
+    m3.ad = "Hakan";
+    m3.soyad = "Kartal";
+           
+    MusteriManager Mngr = new MusteriManager();
+    Mngr.Ekleme(m1,m2,m3);
+    Mngr.Silme(m2);
+    Mngr.Listeleme(m1, m3);
   }
 }
 
 class Musteri{
-  public int Id;
-  public string ad;
-  public string soyad;
+  public int id { get; set; }
+  public string ad { get; set; }
+  public string soyad { get; set; }
 
 }
 
 class MusteriManager{
-  
+  public void Ekleme( params Musteri[] ms){
+    foreach (var i in ms){
+      Console.WriteLine("Müşteri Eklendi: " + i.id +" " + i.ad + " " + i.soyad);
+    }
+    Console.WriteLine("---------------------------------");
+  }
+  public void Silme( params Musteri[] ms){
+    foreach (var i in ms){
+      Console.WriteLine("Müşteri Silindi: " + i.id + " "+i.ad + " " + i.soyad);
+    }
+    Console.WriteLine("---------------------------------");
+  }
+
+
+
+  public void Listeleme(params Musteri[] ms){
+    foreach (var i in ms){
+      Console.WriteLine("Müşteri Listesi : " + i.id +" "+ i.ad + " " + i.soyad);
+    }  
+    Console.WriteLine("---------------------------------");       
+  }
+
 }
