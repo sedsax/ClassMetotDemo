71ecc48930cd479df431eea6cddc18450de92568
Sedanur Savaş <svs.seda60@gmail.com>
2021-02-27 10:55:11 +0000

    yes

--- main.cs
+++ main.cs
@@ -1,7 +1,23 @@
 using System;
+/*
+Bir bankada müşteri takibi yapmak istiyorsunuz.
+Musteri isimli bir Class oluşturunuz. Müşteriye istediğiniz özellikleri ekleyiniz. (Id,Ad,Soyad....)
+MusteriManager sınıfı oluşturunuz. Musteri parametresi alarak Musteri ekleme, listeleme,silme gibi metotları simule ediniz.
+*/
 
 class MainClass {
   public static void Main (string[] args) {
     Console.WriteLine ("Hello World");
   }
+}
+
+class Musteri{
+  public int Id;
+  public string ad;
+  public string soyad;
+
+}
+
+class MusteriManager{
+  
 }
